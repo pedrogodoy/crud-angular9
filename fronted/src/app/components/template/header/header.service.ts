738543574ad97fae684d5e0fdae81931a03f98cc
pedrogodoy <pedrogh.godoy12@gmail.com>
2020-05-28 20:44:30 -0400

    Alterando titulos do header

---
+++ fronted/src/app/components/template/header/header.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HeaderData } from './header-data.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  //Classe que representa um subject, essa classe sempre vai emitir eventos quando houver mudanca
+  private _headerData = new BehaviorSubject<HeaderData>({
+    title: 'Inicio',
+    icon: 'home',
+    routeUrl: ''
+  })
+
+  constructor() { }
+
+  get headerData(): HeaderData {
+    return this._headerData.value
+  }
+
+  set headerData(headerData: HeaderData) {
+    this._headerData.next(headerData)
+  }
+}
